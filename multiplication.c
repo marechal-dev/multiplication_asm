e0e20bdbd861035bad3b386006405b30c8aab08a
marechal-dev <pietro.developer@gmail.com>
2021-09-20 11:40:07 -0300

    Added C implementation and it's compilation to Assembly with GCC

---
+++ multiplication.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  int n1 = 4;
+  int n2 = 2;
+  int multi = 0;
+
+  if (n1 > n2) {
+    do {
+      multi += n1;
+      n2 -= 1;
+    } while (n2 < 0);
+  } else {
+    do {
+      multi += n2;
+      n1 -= 1;
+    } while (n1 < 0);
+  }
+
+  return 0;
+}
